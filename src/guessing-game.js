20dedc472d7d0832d32ca85b17f0f84a66eec5a2
maxhmaen <maxhamen@mail.ru>
2017-02-05 14:53:13 +0300

    completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.value = null;
     }
 
-    guess() {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        this.value = max;
+    
+}
+    
 
+    guess() {
+        
+        this.value = this.min+ Math.ceil((this.max-this.min)/2);
+        return this.value;
     }
 
     lower() {
-
+    this.max= this.value;      
     }
 
     greater() {
-
+this.min=this.value;        
     }
 }
 
